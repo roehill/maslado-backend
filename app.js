0e320029df526be03183fcf1b3e6e942c656c36c
slawomir <roehilldev@gmail.com>
2025-05-23 14:51:07 +0200

    Add version information wjile running server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ dotenv.config();
 // SERVER
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}. Version 1.01`));
 
 // CORS - w przyszlosci zmienic na jedną domenę
 app.use(cors());
